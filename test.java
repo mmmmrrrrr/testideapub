62bc8ee821ab586b26c617106bf6ce8143a08730
mmmmrrrrr <mrw2910@163.com>
2023-10-14 15:05:02 +0800

    改变了hello567

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
+        System.out.println("Hello5");
         System.out.println("Hello6");
+        System.out.println("Hello7");
     }
 }
